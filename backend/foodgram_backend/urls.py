0c17f00f7d3f56915e21679df9e5e2915ae3f6d0
Darya Novogorodskaya <darya.novogorodskaya@yandex.com>
2024-10-17 23:10:01 +0800

    s

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -1,15 +1,12 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('api/', include('api.urls')),
-    path('api/', include('users.urls')),
-    path('admin/', admin.site.urls),
+    path('', include('api.urls',)),
+    path('', include('users.urls',)),
+    path(
+        'redoc/',
+        TemplateView.as_view(template_name='redoc.html'),
+        name='redoc'
+    ),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,4 +1,5 @@
-from django.urls import include, path, re_path
+from django.contrib import admin
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from users.views import FoodgramUserViewSet
@@ -7,6 +8,7 @@ router_v1 = DefaultRouter()
 router_v1.register('users', FoodgramUserViewSet, basename='users')
 
 urlpatterns = [
-    path('', include(router_v1.urls)),
-    re_path(r'auth/', include('djoser.urls.authtoken')),
+    path('admin/', admin.site.urls),
+    path('api/', include(router_v1.urls)),
+    path('api/auth/', include('djoser.urls.authtoken')),
 ]
